858b8eeb21ff529c05a790f3cb537a5a6ef2327a
Kriptikz <Kriptikz@users.noreply.github.com>
2024-01-07 17:06:57 -0600

    add lsp progress to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,20 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'dracula'
-      }
-    })
-  end
+  {
+    "arkav/lualine-lsp-progress"
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require('lualine').setup({
+        options = {
+          theme = 'dracula'
+        },
+        sections = {
+          lualine_c = {
+            'lsp_progress'
+          }
+        }
+      })
+    end
+  }
 }
