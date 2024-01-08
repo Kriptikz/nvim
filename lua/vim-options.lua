cd80d0ee61c64196fc33c56a745ce6e41418796c
Kriptikz <Kriptikz@users.noreply.github.com>
2024-01-07 20:19:22 -0600

    added vim opts colorcolumn 80, linewrap false

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,6 +5,8 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
 vim.wo.relativenumber = true
+vim.opt.colorcolumn = "80"
+vim.opt.wrap = false
 
 --vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 
