63dbe04fc0a56063534bb4082961b2aa0143b8f6
Kriptikz <Kriptikz@users.noreply.github.com>
2024-01-08 00:50:47 -0600

    add vim.g.maplocalleader = ","

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set conceallevel=2")
 vim.g.mapleader = " "
+vim.g.maplocalleader = ","
 
 vim.wo.relativenumber = true
 vim.opt.colorcolumn = "80"
