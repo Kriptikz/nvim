fd7fefb0a576801a380c237e5e7321e9ba2c40ee
Kriptikz <Kriptikz@users.noreply.github.com>
2024-01-07 23:30:40 -0600

    neorg default workspace and conceallevel=2

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,22 +1,23 @@
-return   {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    -- tag = "*",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                personal = "~/notes/personal",
-                work = "~/notes/work",
-              },
+return {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  -- tag = "*",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("neorg").setup {
+      load = {
+        ["core.defaults"] = {},    -- Loads default behaviour
+        ["core.concealer"] = {},   -- Adds pretty icons to your documents
+        ["core.dirman"] = {        -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              personal = "~/notes/personal",
+              work = "~/notes/work",
             },
+            default_workspace = "personal"
           },
         },
-      }
-    end,
-  }
+      },
+    }
+  end,
+}
